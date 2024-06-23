61b157544fd6be9c92b6d8041b72dbb1b1c46c5a
Instructor <dodydev311@gmail.com>
2024-06-23 07:25:27 +0530

    string

--- 5Cases.js
+++ 5Cases.js
@@ -1,7 +1,6 @@
 
 // 0 , 1
 
-
 // 2 => 10 => binary numbers
 
 // 156 => 10011100
@@ -15,10 +14,26 @@
  // " " => 32 
 
 
-// let str = "abhi"
+// let str = "$"
 
-// console.log(str.charCodeAt(2)) 
+// console.log(str.charCodeAt(0)) 
 
-let n = 787
+// let n = 658
 
 // console.log(String.fromCharCode(n)) // A
+
+
+// let ch = "A"  //=> 65 + 32
+// let chI = ch.charCodeAt(0) + 32 // 97
+// console.log(String.fromCharCode(chI)) // a
+
+
+// let ch1 = "a"  //=> 97 - 32
+
+// toUpperCase
+// toLowerCase
+
+let str = "RamesH123$5"
+
+// console.log(str.toUpperCase())
+// console.log(str.toLowerCase())
---
+++ 6CommonFunctions.js
@@ -0,0 +1,29 @@
+
+// trim() 
+
+
+// let str = "   Ram es h   "
+
+// console.log(str, str.length)
+
+// let str1 = str.trim()
+
+// console.log(str1, str1.length)
+
+
+// startsWith()
+// endsWith()
+
+// let str = "Ramesh"
+
+// console.log(str.startsWith("Ka"))
+
+// console.log(str.endsWith("dh"))
+
+
+// includes()
+
+// let str = "Ramesh"
+
+
+// console.log(str.includes("ms")) 
